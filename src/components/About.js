0b7e2d8f111b123cc315796f9f42c706b2a86d2b
Karin Ljunggren <kaljunggren@gmail.com>
2018-10-27 23:52:17 +0200

    About corr

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,8 @@ const About = () => {
                             <div className="card-body">
                                 <p>This is my first React app for learning React.js.</p>
                                 <p>I will build different features for practicing React, ES6 or someting else I find useful.</p>
-                                <p>Many thanks to <a href="https://www.youtube.com/playlist?list=PL4cUxeGkcC9ij8CfkAY2RAGb-tmkNwQHG">The Net Ninja</a> for lots of help and inspiration.</p>
+                                <p>Many thanks to <a href="https://www.youtube.com/playlist?list=PL4cUxeGkcC9ij8CfkAY2RAGb-tmkNwQHG">The Net Ninja</a>
+                                    and to  <a href="https://www.youtube.com/watch?v=l6nmysZKHFU" Academind></a> for lots of help and inspiration.</p>
                             </div>
                         </div>
                     </div>
